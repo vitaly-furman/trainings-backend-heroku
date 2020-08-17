e7f86c479a767e7f2b13e6638cd5ec8213f9b804
vitaly-furman <furman4488@gmail.com>
2020-08-17 23:46:56 +0300

    asdf

--- app/src/config/index.ts
+++ app/src/config/index.ts
@@ -3,12 +3,12 @@ import './dotenv';
 
 const config = {
     service: {
-        port: 8000 || env.get('PORT').required().asPortNumber(),
+        port: process.env.PORT || env.get('PORT').required().asPortNumber(),
     },
     mongo: {
-        uri: "mongodb+srv://user1:user1pass@cluster0.6lbky.mongodb.net/training-system?retryWrites=true&w=majority" || env.get('MONGO_URI').required().asUrlString(),
-        workersCollectionName: "workers" || env.get('MONGO_WORKERS_COLLECTION_NAME').required().asString(),
-        trainingsCollectionName: "trainings" || env.get('MONGO_TRAININGS_COLLECTION_NAME').required().asString(),
+        uri: process.env.MONGO_URI || env.get('MONGO_URI').required().asUrlString(),
+        workersCollectionName: process.env.MONGO_WORKERS_COLLECTION_NAME || env.get('MONGO_WORKERS_COLLECTION_NAME').required().asString(),
+        trainingsCollectionName: process.env.MONGO_TRAININGS_COLLECTION_NAME || env.get('MONGO_TRAININGS_COLLECTION_NAME').required().asString(),
     },
 };
 
